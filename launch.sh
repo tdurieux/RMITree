f5717000aeed5900ba25f7c954a1bb91b7e9ea2c
w1gz <private@mail.org>
2014-04-08 14:53:54 +0200

    Script modified (opens a new terminal) to allow sendMessage & registry to be running at the same time on STDOUT

--- launch.sh
+++ launch.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-function startNoeud {
-	java -jar noeud.jar $1 &
-	sleep 0.3
-}
+## Parameters for fork/fg
+set -m
 
+## Global variables
+TERMINAL="gnome-terminal" # xterm
+
+## Launch registy and store its pid
 java -jar annuaire.jar &
 pid=$!
-sleep 0.3
+# sleep 0.3
 
+## Spawn a fixed number of nodes
 for i in {1..6}
 do
-	startNoeud $i
+    java -jar noeud.jar $1 &
 done
 
-echo "connect 1->2" &> 0
-sleep 0.3
+## Open a new terminal & launch the sendMessage program in it
+## this line will allow sendMessage & registry on stdout without modifying our code
+$TERMINAL -e "java -jar sendMessage.jar" . &
 
-#java -jar sendMessage.jar
-<<EOF
-	
-EOF
+## Retrieve jobs from the pid of the registry and move it forward on the terminal
+jobs $pid
+fg %1
+
+
+#  <<EOF
+# connect 1->2
+# EOF
+
+
 kill `ps -ef | grep "java -jar *noeud.jar" | awk '{print $2}'`
 kill `ps -ef | grep "java -jar *annuaire.jar" | awk '{print $2}'`
-
-
+kill `ps -ef | grep "java -jar *sendMessage.jar" | awk '{print $2}'`
+# killall java

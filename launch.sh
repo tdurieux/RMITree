4061a4e08c6a80b9c35d479cb0f9002e8ce21d92
w1gz <private@mail.org>
2014-04-10 11:13:05 +0200

    restructuration of the launch.sh file

--- launch.sh
+++ launch.sh
@@ -4,34 +4,42 @@
 set -m
 
 ## Global variables
-TERMINAL="gnome-terminal" # xterm
+TERMINAL="terminator" # xterm
+JAR_PATH="jar"
+JDK_PATH="/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.60-2.4.5.1.fc20.x86_64/jre"
 
-## Launch registy and store its pid
-java -jar annuaire.jar &
-pid=$!
-# sleep 0.3
+# start the rmi registry
+$JDK_PATH/bin/rmiregistry &
+rmi_registry_pid=$!
+sleep 0.3
+
+# start the message jar in another terminal
+$TERMINAL -e "java -jar $JAR_PATH/sendMessage.jar" . &
+sleep 0.3
 
 ## Spawn a fixed number of nodes
 for i in {1..6}
 do
-    java -jar noeud.jar $1 &
+    java -jar $JAR_PATH/noeud.jar $i &
 done
+sleep 0.3
 
-## Open a new terminal & launch the sendMessage program in it
-## this line will allow sendMessage & registry on stdout without modifying our code
-$TERMINAL -e "java -jar sendMessage.jar" . &
-
-## Retrieve jobs from the pid of the registry and move it forward on the terminal
-jobs $pid
-fg %1
-
-
-#  <<EOF
-# connect 1->2
-# EOF
+## Launch registy and store its pid
+java -jar $JAR_PATH/annuaire.jar false <<EOF
+connect 1->2
+connect 1->5
+connect 5->6
+connect 2->3
+connect 2->4
+EOF
+sleep 0.3
 
 
-kill `ps -ef | grep "java -jar *noeud.jar" | awk '{print $2}'`
-kill `ps -ef | grep "java -jar *annuaire.jar" | awk '{print $2}'`
-kill `ps -ef | grep "java -jar *sendMessage.jar" | awk '{print $2}'`
-# killall java
+# pid=$!
+# jobs $pid
+# fg %1
+
+# kill `ps -ef | grep "java -jar *noeud.jar" | awk '{print $2}'`
+# kill `ps -ef | grep "java -jar *annuaire.jar" | awk '{print $2}'`
+# kill `ps -ef | grep "java -jar *sendMessage.jar" | awk '{print $2}'`
+killall java

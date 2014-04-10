7c233b129f69c7891f9ffce8dbaa15327b927ff7
w1gz <private@mail.org>
2014-04-10 11:42:57 +0200

    add classpath to the script

--- launch.sh
+++ launch.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-## Parameters for fork/fg
+# Parameters for fork/fg
 set -m
 
-## Global variables
-TERMINAL="terminator" # xterm
+# Global variables
+TERMINAL="gnome-terminal" # xterm
 JAR_PATH="jar"
-JDK_PATH="/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.60-2.4.5.1.fc20.x86_64/jre"
+PWD=`pwd`
+CLASS_PATH="$PWD/jar/annuaire.jar:lille1/car3/durieux_gouzer/rmi"
+REGISTRY_PATH="/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.60-2.4.5.1.fc20.x86_64/jre/bin/rmiregistry 58432"
 
 # start the rmi registry
-$JDK_PATH/bin/rmiregistry &
+export CLASSPATH=$CLASS_PATH
+$REGISTRY_PATH &
 rmi_registry_pid=$!
-sleep 0.3
+sleep 1
 
 # start the message jar in another terminal
 $TERMINAL -e "java -jar $JAR_PATH/sendMessage.jar" . &
 sleep 0.3
 
-## Spawn a fixed number of nodes
+# Spawn a fixed number of nodes
 for i in {1..6}
 do
     java -jar $JAR_PATH/noeud.jar $i &
 done
 sleep 0.3
 
-## Launch registy and store its pid
+# Launch registy and store its pid
 java -jar $JAR_PATH/annuaire.jar false <<EOF
 connect 1->2
 connect 1->5
@@ -34,12 +37,9 @@ connect 2->4
 EOF
 sleep 0.3
 
-
 # pid=$!
 # jobs $pid
 # fg %1
 
-# kill `ps -ef | grep "java -jar *noeud.jar" | awk '{print $2}'`
-# kill `ps -ef | grep "java -jar *annuaire.jar" | awk '{print $2}'`
-# kill `ps -ef | grep "java -jar *sendMessage.jar" | awk '{print $2}'`
-killall java
+# kill everything related to java
+kill `ps -ef | grep "java" | awk '{print $2}'`
